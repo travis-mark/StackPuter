02daa6e70ab80f4540298751df96d56c3334495a
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-05-25 23:15:24 -0400

    Logic cleanup
    Thanks UPE Chapter 8

--- StackPuter/StackPuter/StackPuterApp.swift
+++ StackPuter/StackPuter/StackPuterApp.swift
@@ -18,48 +18,49 @@ class Stack {
         stack.append(s)
     }
 }
+let constants: [String: Double] = [
+    "PI": 3.14159265358979323846,        // π
+    "E": 2.71828182845904523536,         // Base of natural logarithms
+    "GAMMA": 0.57721566490153286060,     // Euler-Mascheroni constant
+    "DEG": 57.29577951308232087680,      // Degrees per radian
+    "PHI": 1.61803398874989484820,       // Golden ratio
+]
 
-let monadic = ["abs", "sqrt"]
-func apply(_ operation: String, _ num: Int) -> Int {
-    switch operation {
-    case "abs":
-        return abs(num)
-    case "sqrt":
-        return Int(sqrt(Double(num)))
-    default:
-        return 0
-    }
-}
-let dyadic = ["+", "-", "*", "/"]
-func apply(_ operation: String, _ num1: Int, _ num2: Int) -> Int {
-    switch operation {
-    case "+":
-        return num1 + num2
-    case "-":
-        return num1 - num2
-    case "*":
-        return num1 * num2
-    case "/":
-        return num1 / num2
-    default:
-        return 0
-    }
-}
+let monadic: [String: (Double) -> Double] = [
+    "abs": { abs($0) },
+    "sqrt": { sqrt($0) },
+    "sin": { sin($0) },
+    "cos": { cos($0) },
+    "tan": { tan($0) },
+    "asin": { asin($0) },
+    "acos": { acos($0) },
+    "atan": { atan($0) },
+    "exp": { exp($0) },
+    "log": { log($0) },
+]
+let dyadic: [String: (Double, Double) -> Double]  = [
+    "+": { $0 + $1 },
+    "-": { $0 - $1 },
+    "*": { $0 * $1 },
+    "/": { $0 / $1 },
+]
 
 func eval(_ forms: String) -> [String] {
     let stack = Stack()
     let scanner = Scanner(string: forms)
     
     while !scanner.isAtEnd, let token = scanner.scanUpToCharacters(from: .whitespaces) {
-        if (token.range(of: #"^-?\d+$"#, options: .regularExpression) != nil) {
+        if (token.range(of: #"^-?\d+(.\d+)?$"#, options: .regularExpression) != nil) {
             stack.push(token)
-        } else if monadic.contains(token) {
-            let val = Int(stack.pop())!
-            stack.push(String(apply(token, val)))
-        } else if dyadic.contains(token) {
-            let lhs = Int(stack.pop())!
-            let rhs = Int(stack.pop())!
-            stack.push(String(apply(token, lhs, rhs)))
+        } else if let monad = monadic[token] {
+            let val = Double(stack.pop())!
+            stack.push(String(monad(val)))
+        } else if let dyad = dyadic[token] {
+            let lhs = Double(stack.pop())!
+            let rhs = Double(stack.pop())!
+            stack.push(String(dyad(lhs, rhs)))
+        } else {
+            assert(false, "Unrecognized symbol: \(token)")
         }
     }
     return stack.stack
